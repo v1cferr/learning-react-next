eed8d92ac764796aa20bfe2c9ceeda57b6957cc6
victor <dev.victorferreira@gmail.com>
2024-05-17 15:14:38 -0300

    docs: LikeButton component

--- 5-next/app/like-button.js
+++ 5-next/app/like-button.js
@@ -2,6 +2,14 @@
 
 import { useState } from "react";
 
+/**
+ * Componente LikeButton:
+ * - Renderiza um botão que aumenta o contador de curtidas.
+ * - Utiliza o hook useState para gerenciar o estado local do botão (número de curtidas).
+ * - A atualização da interface ocorre somente no lado do cliente.
+ *
+ * @returns {JSX.Element} Um botão que mostra o número de curtidas.
+ */
 export default function LikeButton() {
   // Inicializa o estado "likes" com o valor 0
   const [likes, setLikes] = useState(0);
